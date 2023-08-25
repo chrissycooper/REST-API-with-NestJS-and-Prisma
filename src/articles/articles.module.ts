a0154537bbed50011766c4fe1374d00171edd136
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-08-25 15:58:59 -0500

    Import Prisma Client to Articles Module

--- src/articles/articles.module.ts
+++ src/articles/articles.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ArticlesService } from './articles.service';
 import { ArticlesController } from './articles.controller';
+import { PrismaClient } from '@prisma/client';
 
 @Module({
   controllers: [ArticlesController],
   providers: [ArticlesService],
+  imports: [PrismaClient],
 })
 export class ArticlesModule {}
